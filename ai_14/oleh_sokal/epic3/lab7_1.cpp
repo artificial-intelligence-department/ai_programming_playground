02e357f3570ae62dac7ce8c6c306f19a312538d4
GoodKn1ght <Oleh.Sokal.ShI.2023@lpnu.ua>
2023-11-28 09:36:18 +0200

    deletion of epic1 and correction of codes

--- ai_14/oleh_sokal/epic3/lab7_1.cpp
+++ ai_14/oleh_sokal/epic3/lab7_1.cpp
@@ -1,73 +1,43 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
 
-int min_element(int n, int a[]) {
-    int min = a[0];
-    for (int i = 1; i < n; i++)
-        if (a[i] < min)
-            min = a[i];
-    return min;
+int pointInCircle(double x, double y, double R) {
+    double distance = sqrt(x * x + y * y);
+    return distance <= R;
 }
 
-int findMinWordLength(const int arr[], int n) {
-    int min = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (min > arr[i]) {
-            min = arr[i];
+int belong(int R, int numPoints, ...) {
+    int count = 0;
+
+    va_list args;
+    va_start(args, numPoints);
+
+    for (int i = 0; i < numPoints; i++) {
+        double x = va_arg(args, double);
+        double y = va_arg(args, double);
+
+        if (pointInCircle(x, y, R)) {
+            count++;
         }
     }
-    return min;
+
+    va_end(args);
+
+    return count;
 }
 
 int main() {
-    int var;
-    cout << "яке завдання:(1,2)";
-    cin >> var;
+    int R = 100;
 
-    if (var == 2) {
-        int n = 0;
-        std::string sentence;
-        cin.ignore();
-        getline(std::cin, sentence);
+    int count1 = belong(R, 3, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0);
+    printf("Number of points belonging to the circle with 3 parameters: %d\n", count1);
 
-        int count = 0;
-        int arr[100] = {0};
+    int count2 = belong(R, 5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0);
+    printf("Number of points belonging to the circle with 9 parameters: %d\n", count2);
 
-        for (int i = 0; i < sentence.length(); i++) {
-            if (sentence[i] != ' ' && sentence[i] != '\0') {
-                count++;
-            } else {
-                arr[n] = count;
-                count = 0;
-                n++;
-            }
-        }
-
-        int min = findMinWordLength(arr, n);
-
-        std::cout << "Length of smallest word: " << min << std::endl;
-
-        return 0;
-    }
-
-    if (var == 1) {
-        int a[100];
-        int n;
-
-        cout << "введіть кількість чисел: ";
-        cin >> n;
-
-        for (int i = 0; i < n; i++) {
-            cout << "введіть число " << i + 1 << ": ";
-            cin >> a[i];
-        }
-
-        int result = min_element(n, a);
-        cout << "Мінімальний елемент: " << result << endl;
-
-        return 0;
-    }
+    int count3 = belong(R, 6, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0);
+    printf("Number of points belonging to the circle with 11 parameters: %d\n", count3);
 
     return 0;
 }
