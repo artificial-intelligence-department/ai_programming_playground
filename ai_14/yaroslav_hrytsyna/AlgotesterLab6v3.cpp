9df58917baa442dba6bf445e8137e0251edbfbb2
yaroslavhrytsyna <146413637+yaroslavhrytsyna@users.noreply.github.com>
2023-12-12 19:18:59 +0200

    Update AlgotesterLab6v3.cpp

--- ai_14/yaroslav_hrytsyna/AlgotesterLab6v3.cpp
+++ ai_14/yaroslav_hrytsyna/AlgotesterLab6v3.cpp
@@ -43,10 +43,9 @@ int main()
             continue;
         }
         for (int j = 0; j < N; ++j) {
-            int f=0;
             if (grid[k[i][0]][j] != 0) {
                 nums[grid[k[i][0]][j]-1]=0;
-                f=1;
+                c--;
             }
             if (grid[j][k[i][1]] != 0){
                 nums[grid[j][k[i][1]]-1]=0;
