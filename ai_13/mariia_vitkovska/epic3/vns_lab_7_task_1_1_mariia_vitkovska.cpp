f611c38a6f2413e36cd2315c37d1eae2238aa069
marik 1 <mariia.vitkovska.2023@lpnu.ua>
2023-12-12 22:09:07 +0200

    .

--- ai_13/mariia_vitkovska/epic3/vns_lab_7_task_1_1_mariia_vitkovska.cpp
+++ ai_13/mariia_vitkovska/epic3/vns_lab_7_task_1_1_mariia_vitkovska.cpp
@@ -6,11 +6,16 @@ using namespace std;
 double length( double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
 }
+
+int length(int x, int y, int t) {
+    return (x+y+t);
+}
 int main() {
     double x1, y1, x2, y2;
     cout << "enter the coordinates of the 1st point: " << endl;
     cin >> x1 >> y1;
     cout << "enter the coordinates of the 2nd point: " << endl;
     cin >> x2 >> y2;
-    cout << length(x1, y1, x2, y2);
+    cout << length(x1, y1, x2, y2) << endl;
+    cout << length(2, 2, 6);
 }
