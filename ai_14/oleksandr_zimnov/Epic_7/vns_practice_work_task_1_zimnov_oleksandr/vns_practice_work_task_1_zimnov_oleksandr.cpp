7f4cc9f31ce00a376ab5fb1354acc35bb9a7635c
Zimnov1 <oleksandr.zimnov.shi.2023@lpnu.ua>
2023-12-13 23:11:26 +0200

    Add change

--- ai_14/oleksandr_zimnov/Epic_7/vns_practice_work_task_1_zimnov_oleksandr/vns_practice_work_task_1_zimnov_oleksandr.cpp
+++ ai_14/oleksandr_zimnov/Epic_7/vns_practice_work_task_1_zimnov_oleksandr/vns_practice_work_task_1_zimnov_oleksandr.cpp
@@ -1,18 +1,36 @@
-#include <iostream> // used a library iostream to output and input value. This do with function std::cout and std::cin
-#include <cmath> //used a library cmath to use mathematical functions such as sqrt and round
+#include <iostream> 
+#include <cstdarg>
+#include <cmath> 
 
-using namespace std; // a directive that tells the compiler to use a namespace named std
-int main() { // create a function named main
-    double ha, hb, hc; // creating variables that are responsible for the results of the heights of their corresponding sides. Type double use to fractional number
-    int a = 6, b = 5, c = 3; // creating sides variables. Type int use to whole numbers
-    double p; // the variable sum of the sides of the triangle is divided by two
-    p = (a + b + c) / 2; //calculation of the value of p according to the formula
-    ha = round((2.0 / a) * sqrt(p * (p - a) * (p - b) * (p - c)) * 10) / 10; // calculation of the value of height drawn from the side a according to the formula
-    hb = round((2.0 / b) * sqrt(p * (p - a) * (p - b) * (p - c)) * 10) / 10; // calculation of the value of height drawn from the side b according to the formula
-    hc = round((2.0 / c) * sqrt(p * (p - a) * (p - b) * (p - c)) * 10) / 10; // calculation of the value of height drawn from the side c according to the formula
+using namespace std; 
+ 
+void print_helper(int count, int temp, va_list args) { 
+    if (temp < count) {
+        double currentArg = va_arg(args, double);
+        temp++;
+        cout << "The length of the height drawn from the side " << temp << " is: " << currentArg << endl;
+        print_helper(count, temp, args); //15. в коді використано рекурсивну функцію 
+    }
+}
+
+void print(int count, int temp, ...) { //13. в коді використано еліпсис 
     cout << "Side of triangle are: 5, 7 and 6" << endl;
-    cout << "The length of the height drawn from the side a: " << ha << endl; // output height drawn from the side a
-    cout << "The length of the height drawn from the side b: " << hb << endl; // output height drawn from the side b
-    cout << "The length of the height drawn from the side c: " << hc << endl; // output height drawn from the side c
-    //function round use to round a number, sqrt use to found the root of a number, cout to output information, cin to input information, endl to new line
+    va_list args;
+    va_start(args, temp);
+    print_helper(count, temp, args);
+    va_end(args);
+}
+int main() { 
+    double mas[3][2]; //7. в коді використаний двовимірний масив 
+    int a = 6, b = 5; //1. в коді використана як мінімум одна цілочисельна змінна
+    const int c = 3; //4. в коді використана як мінімум одна цілочисельна константа
+    float p; //2. в коді використана як мінімум одна дійсна змінна
+    mas[0][1] = a;
+    mas[1][1] = b;
+    mas[2][1] = c;
+    p = (a + b + c) / 2; //17. в коді використано математичні операції та математичні функції
+    for(int i=0; i<3; i++){ //10. в коді використаний for цикл 
+        mas[i][0] = round((2.0 / mas[i][1]) * sqrt(p * (p - a) * (p - b) * (p - c)) * 10) / 10; 
+    }
+    print(3, 0, mas[0][0], mas[1][0], mas[2][0]); //16. в коді використано параметри та аргументи функції
 }
--- ai_14/oleksandr_zimnov/Epic_7/vns_practice_work_task_4_zimnov_oleksandr/vns_practice_work_task_4_zimnov_oleksandr.cpp
+++ ai_14/oleksandr_zimnov/Epic_7/vns_practice_work_task_4_zimnov_oleksandr/vns_practice_work_task_4_zimnov_oleksandr.cpp
@@ -5,11 +5,16 @@ int main() {
         int n, number, sum=0;
         cout << "Enter n: ";
         cin >> n;
-        for(int i=0; i<n; i++){
-            cout<<"Enter "<<i+1<<" number: ";
+        int i=0;
+        do{ //8. в коді використаний do while цикл
+            i++;
+            cout<<"Enter "<<i<<" number: ";
             cin>>number;
-            if(number%2==0) //the if function is used for comparison, only if done correctly the execution of the part of the code is executed. == means comparison. % means division without a remainder
+            if(number%2!=0) //5. в коді використані умовні оператори та розгалуження
+            break; //11. в коді використано оператори break і continue
             sum+=number;
+            continue; 
         }
+        while(i<n);
         cout<<"Your sum is: "<<sum;
 }
