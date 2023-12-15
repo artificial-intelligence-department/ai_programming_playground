fd78224405698b8ae6c774a65900fcbc9824af1b
Taras008 <Taras.Dakh.ShI.2023@lpnu.ua>
2023-12-15 12:15:42 +0200

    Added true file code

--- ai_11/taras_dakh/Epic7/code2.cpp
+++ ai_11/taras_dakh/Epic7/code2.cpp
@@ -1,135 +1,181 @@
 #include <iostream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
+#include <fstream>
+#include <cmath>
 
-const int pull_request_number = 70;  // Замініть на свій номер пулл реквесту
+const int pull_request_number = 1021;
 
-// Крок 1
-void fillMatrix(double matrix[10][10]) {
+// Функція для створення матриці з плаваючою крапкою
+double** createFloatingMatrix() {
+    double** matrix = new double*[10];
     for (int i = 0; i < 10; ++i) {
+        matrix[i] = new double[10];
         for (int j = 0; j < 10; ++j) {
-            matrix[i][j] = sin(i * j * pull_request_number);  // Замість sin можна використовувати іншу математичну функцію
+            matrix[i][j] = sin(i) * cos(j);
         }
     }
+    return matrix;
 }
 
-void fillMatrix(int matrix[10][10]) {
+// Функція для створення матриці цілих чисел
+int** createIntegerMatrix() {
+    int** matrix = new int*[10];
     for (int i = 0; i < 10; ++i) {
+        matrix[i] = new int[10];
         for (int j = 0; j < 10; ++j) {
-            matrix[i][j] = abs(cos(i * j * pull_request_number));  // Замість abs можна використовувати іншу математичну функцію
+            matrix[i][j] = static_cast<int>(floor(exp(i) * sqrt(j)));
         }
     }
+    return matrix;
 }
+// Константа для назви файлу
+const std::string fileName = "output.txt";
 
-// Крок 2
-void sortMatrix(double matrix[10][10], double pull_request_value) {
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            for (int k = 0; k < 9 - j; ++k) {
-                if (matrix[k][i] > matrix[k + 1][i]) {
-                    std::swap(matrix[k][i], matrix[k + 1][i]);
+// Шаблон функції для сортування стовпців матриці з числами з плаваючою крапкою
+template <typename T>
+void sortColumns(T** matrix, int rows, int cols) {
+    for (int j = 0; j < cols; ++j) {
+        for (int i = 0; i < rows - 1; ++i) {
+            for (int k = 0; k < rows - i - 1; ++k) {
+                if (matrix[k][j] > matrix[k + 1][j]) {
+                    std::swap(matrix[k][j], matrix[k + 1][j]);
                 }
             }
         }
     }
 }
 
-void sortMatrix(int matrix[10][10], int pull_request_value) {
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            for (int k = 0; k < 9 - j; ++k) {
-                if (matrix[i][k] < matrix[i][k + 1]) {
-                    std::swap(matrix[i][k], matrix[i][k + 1]);
-                }
-            }
+// Шаблон функції для виведення матриці
+template <typename T>
+void printMatrix(T** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << matrix[i][j] << " ";
         }
+        std::cout << std::endl;
     }
 }
 
-// Крок 3
-void calculateSums(double matrix[10][10]) {
-    int sums[10];
-    srand(time(0));
+// Функція для обчислення сум 3-х або 4-х рандомно взятих елементів у кожному стовпці
+// та створення нового цілочисельного статичного масиву з 10 елементів
+int* calculateColumnSums(double** matrix, int rows, int cols) {
+    int* columnSums = new int[10];
 
-    for (int i = 0; i < 10; ++i) {
-        sums[i] = 0;
-        for (int j = 0; j < 4; ++j) {
-            int randomIndex = rand() % 10;
-            sums[i] += matrix[randomIndex][i];
+    for (int j = 0; j < cols; ++j) {
+        double sum = 0.0;
+
+        // Вибір 3-х або 4-х рандомних елементів у стовпці
+        int elementsToSum = (rand() % 2) + 3;
+
+        for (int k = 0; k < elementsToSum; ++k) {
+            sum += matrix[k][j];
         }
 
-        // Якщо сума має дробову частину, вона відкидається
-        sums[i] = static_cast<int>(sums[i]);
+        // Додавання суми до масиву, відкидаючи дробову частину
+        columnSums[j] = static_cast<int>(sum);
     }
 
-    // Крок 4
-    writeToFile(sums);
-
-    // Крок 5
-    calculateProduct();
+    return columnSums;
 }
 
-// Крок 4
-void writeToFile(int sums[10]) {
-    std::ofstream outFile("result_file.txt");
+// Функція для запису масиву у файл
+bool writeArrayToFile(const int* array, int size, const std::string& filename) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        for (int i = 0; i < size; ++i) {
+            file << array[i] << " ";
+        }
+        file.close();
+        return true;
+    }
+    return false;
+}
 
-    for (int i = 0; i < 10; ++i) {
-        outFile << sums[i] << std::endl;
+// Функція для відкриття файлу та розрахунку добутку його елементів
+int calculateProductFromFile() {
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file." << std::endl;
+        return 0;
     }
 
-    outFile.close();
-}
-
-// Крок 5
-void calculateProduct() {
-    std::ifstream inFile("result_file.txt");
     int product = 1;
     int value;
-
-    while (inFile >> value) {
+    while (file >> value) {
         product *= value;
     }
 
-    // Виведення результату в консоль
-    std::cout << "Product: " << product << std::endl;
-
-    inFile.close();
+    file.close();
+    return product;
 }
 
-// Крок 6
-void deleteFile() {
-    if (remove("result_file.txt") != 0) {
-        perror("Error deleting file");
+// Функція для видалення файлу
+void deleteFile(const std::string& filename) {
+    if (remove(filename.c_str()) == 0) {
+        std::cout << "File deleted successfully." << std::endl;
     } else {
-        std::cout << "File deleted successfully" << std::endl;
+        std::cerr << "Failed to delete file." << std::endl;
     }
 }
 
-// Головна функція
+// Головна функція, яка викликає всі інші функції
+void mainFunction() {
+    const int rows = 10;
+    const int cols = 10;
+
+    // Створення матриці з числами з плаваючою крапкою
+    double** floatingMatrix = createFloatingMatrix();
+
+    // Виведення початкової матриці
+    std::cout << "Original Floating Matrix:" << std::endl;
+    printMatrix(floatingMatrix, rows, cols);
+
+    // Виклик функції для сортування стовпців
+    sortColumns(floatingMatrix, rows, cols);
+
+    // Виведення відсортованої матриці
+    std::cout << "Sorted Floating Matrix:" << std::endl;
+    printMatrix(floatingMatrix, rows, cols);
+
+    // Виклик функції для обчислення сум та створення нового масиву
+    int* sumsArray = calculateColumnSums(floatingMatrix, rows, cols);
+
+    // Виведення нового масиву сум
+    std::cout << "Column Sums Array:" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        std::cout << sumsArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Запис масиву у файл
+    if (writeArrayToFile(sumsArray, 10, fileName)) {
+        std::cout << "Array successfully written to file." << std::endl;
+    } else {
+        std::cout << "Failed to write array to file." << std::endl;
+    }
+
+    // Виклик функції для відкриття файлу та розрахунку добутку елементів
+    int product = calculateProductFromFile();
+
+    // Виведення добутку елементів у консоль
+    std::cout << "Product of array elements in file: " << product << std::endl;
+
+    // Виклик функції для видалення файлу
+    deleteFile(fileName);
+
+    // Видалення виділеної пам'яті для матриці та масиву сум
+    for (int i = 0; i < rows; ++i) {
+        delete[] floatingMatrix[i];
+    }
+    delete[] floatingMatrix;
+
+    delete[] sumsArray;
+}
+
 int main() {
-    double doubleMatrix[10][10];
-    int intMatrix[10][10];
-
-    // Визначення типу матриці (з числами з плаваючою крапкою або цілими числами)
-    if (pull_request_number % 2 == 0) {
-        fillMatrix(doubleMatrix);
-        sortMatrix(doubleMatrix, pull_request_number * 1.234);  // Замініть 1.234 на ваше значення з плаваючою крапкою
-    } else {
-        fillMatrix(intMatrix);
-        sortMatrix(intMatrix, pull_request_number * 1.234);
-    }
-
-    // Крок 2
-    // Для варіанту (a) можна використовувати sortMatrix(doubleMatrix, pull_request_number * 1.234);
-    // Для варіанту (b) можна використовувати sortMatrix(intMatrix, pull_request_number * 1.234);
-
-    // Крок 3, 4 та 5
-    calculateSums(doubleMatrix);
-
-    // Крок 6
-    deleteFile();
+    // Виклик головної функції
+    mainFunction();
 
     return 0;
 }
