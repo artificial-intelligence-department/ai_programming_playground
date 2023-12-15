d1f7e983b8896de50ffe7d47552ebb15622a2672
Yurii Hrabovskyi <yurii.hrabovskyi.shi.2023@lpnu.ua>
2023-12-15 10:00:43 +0200

    Add file

---
+++ ai_12/yurii_hrabovskiy/Epic 7/TASK.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<smath>
+
+using namespace std;
+
+double f1()
+    {
+
+        double matrix[10][10];
+
+        for(int i=0; i<10; i++)
+        {
+            for 
+        }
+    
+    
+    
+    
+    
+    
+    }
