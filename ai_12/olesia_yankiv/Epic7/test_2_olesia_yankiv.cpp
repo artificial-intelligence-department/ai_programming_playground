c9a46626a1301e431ce4fcc48d8217df64b1236b
Olesia Yankiv <146074677+OlesiaYankiv@users.noreply.github.com>
2023-12-19 09:50:11 +0200

    Update test_2_olesia_yankiv.cpp

--- ai_12/olesia_yankiv/Epic7/test_2_olesia_yankiv.cpp
+++ ai_12/olesia_yankiv/Epic7/test_2_olesia_yankiv.cpp
@@ -11,10 +11,11 @@ int main()
     string lastName; //string variable
     cin>>lastName; //input and output
     
-    int numLetter = static_cast <int> (fieldArea / widthWithLetters);
+    
 
     double widthWithLetters = lastName. length()* 0.8; // double variable
     double fieldArea = 110*3*12;
+    int numLetter = static_cast <int> (fieldArea / widthWithLetters);
     double centralConvert = 3* 39.3701; // 3 meters converted to inches
 
   // here we will describe the program how to read text in both cases (left to right and right to left)
@@ -50,11 +51,19 @@ int main()
     //}
     //cout<<endl;
 
-    cout<<"letters that will fit in that field size"<<endl;
-    for (int i =0; i<numLetter && i<lastName.length(); ++i)
+    cout<<"Letters that will fit in that field size"<<endl; //if-else clause
+     if (numLetter >= lastName.length())
     {
-        cout <<lastName[i];
+        for (int i = 0; i < lastName.length(); ++i) //for function
+        {
+            cout << lastName[i];
+        }
     }
+    else
+    {
+        cout << "Error! Your name does not fit" << endl;
+    }
+    cout << endl;
  cout<<endl;
  return 0;
 
