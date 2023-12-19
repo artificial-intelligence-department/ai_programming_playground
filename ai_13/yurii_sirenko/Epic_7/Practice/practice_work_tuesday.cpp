39df966de7514590aba8f67cc4e2ebcde17b96aa
YuraSirenk <yurii.sirenko.shi.2023@lpnu.ua>
2023-12-19 10:11:57 +0200

    19.12.2023 - final work with caption for code

--- ai_13/yurii_sirenko/Epic_7/Practice/practice_work_tuesday.cpp
+++ ai_13/yurii_sirenko/Epic_7/Practice/practice_work_tuesday.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-int countVowels(string str) 
+int countVowels(string str) //14 в коді використано параметри та аргументи функції
 {
     int count = 0;
     for (int i = 0; str[i] != '\0'; ++i) 
@@ -27,7 +27,7 @@ int main()
     const double widthPerVowel = 0.8; //3 в коді використана як мінімум одна дійсний з подвійною точністю змінна
 
     int nameLength = 0;
-    for (int i = 0; myLastName[i] != '\0'; ++i) {
+    for (int i = 0; myLastName[i] != '\0'; ++i) { //10 в коді використаний for цикл 
         nameLength++;
     }
 
@@ -39,7 +39,7 @@ int main()
     std::cout << "lenght of banner : " << bannerLength << " m" << std::endl;
 
     int number_ofelements_pole = lenghtPole/letterWidth;
-    char pole[number_ofelements_pole];
+    char pole[number_ofelements_pole]; //6 в коді використаний одновимірний масив 
     for(int i = 0;i<number_ofelements_pole;i+=nameLength)
     {
         for(int j = 0;j<nameLength;j++)
@@ -59,9 +59,9 @@ int main()
     {
         if(i>middleIndex-1.5&&i<middleIndex+1.5+letterWidth)
         {
-            cout<<pole[j];
+            cout<<pole[j]; //18 в коді використано оператори виведення та введення даних
         }
-    }
+    } 
 
 
     return 0;
