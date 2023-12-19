dff86a8f78f7ae3d776526bfc203b0c6d1d1fe16
SerhiiKysil <serhii.kysil.shi.2023@lpnu.ua>
2023-12-19 09:56:47 +0200

    Remade task

--- ai_14/serhii_kysil/Epic7/321.cpp
+++ ai_14/serhii_kysil/Epic7/321.cpp
@@ -6,7 +6,7 @@ int main() {
     // Визначення прізвища та кількості голосних букв
     std::string surname;
     std::cin >> surname;
-    int numVowels = 0;
+    int numVowels = 0; // пункт 1
     for (char letter : surname) {
         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
             letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'y' || letter == 'Y') {
@@ -15,10 +15,10 @@ int main() {
     }
 
     // Розрахунок ширини букви
-    double letterWidth = numVowels * 0.8; // метри
+    double letterWidth = numVowels * 0.8; // пункт 3
 
     // Довжина поля у метрах
-    double fieldLength = 118.872; // метри
+    double fieldLength = 118.872; 
 
     // Визначення проміжку для написання
     double writingSpace = 3.0; // метри
@@ -35,7 +35,7 @@ int main() {
     int centralEnd = centralStart + numCharacters;
 
     // Виведення символів на 3-х центральних метрах поля
-    for (int i = centralStart; i < centralEnd; i++) {
+    for (int i = centralStart; i < centralEnd; i++) {// пункт 10
         std::cout << surname[i % surname.length()];
     }
     std::cout << std::endl;
