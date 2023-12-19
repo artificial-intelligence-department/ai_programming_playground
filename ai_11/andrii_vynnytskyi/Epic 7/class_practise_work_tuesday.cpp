a46416cceda9af5ed892f1f1df2d1526773492b3
AndrewVynnytskyi <nikoljavin@gmail.com>
2023-12-19 09:59:07 +0200

    Add structure to the work and readiang from file

--- ai_11/andrii_vynnytskyi/Epic 7/class_practise_work_tuesday.cpp
+++ ai_11/andrii_vynnytskyi/Epic 7/class_practise_work_tuesday.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
+
+struct CI
+{
+    const double duim = 2.54;//константна дійсна змінна з подвійною точністю для запису скільки дюймів є сантиметр
+    double lenght;
+    double lenght_m = calculations();
+
+
+double calculations()
+{
+    return (lenght * 3 * 12 * duim)/100;
+}
+
+
+};
 
 int count_sym(string text)
 {
-    int count = 0;
+    int count = 0;//Цілочисельна змінна для підрахунку символів
     char arr[] = {'a', 'e', 'i', 'o', 'u', 'y'};
     for(int i = 0; i < 6; i++)
     {
@@ -47,17 +63,37 @@ string medium(string text, double lenght_f , double lenght,  double lenght_of_s)
     }
     return result;
 }
+string read(const string& path)
+{
+    string output;
+    string line;
+    ifstream file;
+    // Відкриття файлу для зчитування
+    file.open(path, ios_base::app);
+    if (!file){
 
+        return "Failure" ;
+
+    }
+// Зчитування рядків з файлу та їх об'єднання
+    while(getline(file , line))
+    {
+        output += line;
+
+    }
+// Закриття файлу
+    file.close();
+    return output;
+}
 
 int main() {
-    const double duim = 2.54;
-    const int lenght = 100;
-    double lenght_m = (lenght * 3 * 12 * duim)/100;
-    string name = "Vynnytskyi";
+    CI len;
+    len.lenght = 100;
+    string name = read("C:/Users/Nout_1/CLionProjects/untitled7/output.txt");
     double interval = count_sym(name) * 0.8;
     double size = name.size() * interval;
 
-    cout<< medium(name,lenght_m, size, interval);
-    
+    cout<< medium(name,len.calculations(), size, interval);
+
     return 0;
 }
