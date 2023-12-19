03b741ae68f086001d005ea081233786d92cc6b2
YuraSirenk <yurii.sirenko.shi.2023@lpnu.ua>
2023-12-19 09:52:09 +0200

    19.12.2023 added function to get middle symbol

--- ai_13/yurii_sirenko/Epic_7/Practice/practice_work_tuesday.cpp
+++ ai_13/yurii_sirenko/Epic_7/Practice/practice_work_tuesday.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 int countVowels(string str) 
 {
     int count = 0;
@@ -36,6 +37,31 @@ int main()
 
 
     std::cout << "lenght of banner : " << bannerLength << " m" << std::endl;
+
+    int number_ofelements_pole = lenghtPole/letterWidth;
+    char pole[number_ofelements_pole];
+    for(int i = 0;i<number_ofelements_pole;i+=nameLength)
+    {
+        for(int j = 0;j<nameLength;j++)
+        {
+            pole[i+j]=myLastName[j];
+        }
+    }
+
+    // Знаходимо середній індекс
+    double middleIndex = number_ofelements_pole / 2;
+
+    // Виводимо символи на 3-х центральних метрах
+    std::cout << "middle symbols ";
+
+    int j = 0;
+    for(double i = 0;i<lenghtPole;j++,i+=letterWidth)
+    {
+        if(i>middleIndex-1.5&&i<middleIndex+1.5)
+        {
+            cout<<pole[j];
+        }
+    }
 
     return 0;
 }
