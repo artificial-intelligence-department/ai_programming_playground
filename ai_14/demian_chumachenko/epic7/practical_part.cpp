dcd51dd4b91b7db4bc2efe6295e7d6fcd988897b
DamianChum <demian.chumachenko.shi.2023@lpnu.ua>
2023-12-19 09:59:47 +0200

    final

--- ai_14/demian_chumachenko/epic7/practical_part.cpp
+++ ai_14/demian_chumachenko/epic7/practical_part.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+
 /*Загальні відомості
 В американській системі виміру довжин використовуються ярди, фути, дюйми. 1 Ярд = 3 фути. 1 Фут = 12 дюймів. 1 Дюйм = 2.54 см.
 Футбольні поля мають можуть мати різну довжину
@@ -13,6 +14,12 @@
 
 using namespace std;
 
+struct Mid{ // struct 19
+    double center;
+    double left;
+    double right;
+};
+
 int main(){
     string surname = "Chumachenko";
 
@@ -24,10 +31,6 @@ int main(){
     float letter_with = 0; 
 
     const float letter_with_coff = 0.8;
-
-    //const float duym = 2.54;
-    //const float foot = 12 * duym;
-    //const float yard = 3 * foot;
 
     const float yard = 0.9144; // in meters
 
@@ -47,21 +50,37 @@ int main(){
 
     int amount_of_let = game_length_c / letter_with; 
 
-    double center = game_length_c / 2;
-    double left = center - 3;
-    double right = center + 3;
+    Mid Cent;
 
+    Cent.center = game_length_c / 2;
+    Cent.left = Cent.center - 3;
+    Cent.right = Cent.center + 3;
+
+    ofstream file("output1.txt"); // output file 16
+    file << "при написанні прізвища з лівого краю поля —>  до правого" << endl;
     int lett = 0;
     for(float i = 0; i < game_length_c; i += letter_with){
         lett++;
-        if((i + letter_with) >= left && (i + letter_with) <= right || i >= left && i <= right || (i - letter_with) >= left && (i - letter_with) <= right){
+        if((i + letter_with) >= Cent.left && (i + letter_with) <= Cent.right || i >= Cent.left && i <= Cent.right || (i - letter_with) >= Cent.left && (i - letter_with) <= Cent.right){
             cout << surname[lett%surname.length()] << " " << i << endl;
+            file << surname[lett%surname.length()] << " " << i << endl;
         }
     }
+    file.close();
+
+    ofstream file1("output2.txt");
+    file << "при написанні прізвища з правого краю поля <—  до лівого." << endl;
+
+    lett = amount_of_let;
+    for(float i = game_length_c; i > 0; i -= letter_with){
+        lett--;
+        if((i + letter_with) >= Cent.left && (i + letter_with) <= Cent.right || i >= Cent.left && i <= Cent.right || (i - letter_with) >= Cent.left && (i - letter_with) <= Cent.right){
+            cout << surname[lett%surname.length()] << " " << i << endl;
+            file1 << surname[lett%surname.length()] << " " << i << endl;
+        }
+    }
+    file1.close();
     
-    cout << left << endl;
-    cout << center << endl;
-    cout << right << endl;
 
     return 0;
 }
