a418f49c9c2f2141e1944a3f40ebb12556f5c8fd
mykytanpdl <mykyta.napadailo.shi.2023@lpnu.ua>
2023-12-19 09:33:51 +0200

    Added tuesday class work

---
+++ ai_14/mykyta_napadailo/epic_7/lecture_practice_tuesday.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    const int group = 14; // Вимога 14: використав цілочисельної константи для збереження незмінного числа
+    int field_length = group * 10 - 10;
+    string surname = "Napadailo";
+    
+
+}
