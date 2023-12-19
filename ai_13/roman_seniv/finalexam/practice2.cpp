0560925b8923a270a3d0cbed4e95686548304f2c
RomanSeniv2005 <146375426+RomanSeniv2005@users.noreply.github.com>
2023-12-19 09:57:04 +0200

    Update practice2.cpp

--- ai_13/roman_seniv/finalexam/practice2.cpp
+++ ai_13/roman_seniv/finalexam/practice2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 double calculateSize(const string& surname) {
     int count = 0;
-    for (char letter : surname) {
+    for (char letter : surname) {   // використовую цикл for 
         if (tolower(letter) == 'a' || tolower(letter) == 'e' || tolower(letter) == 'i' ||
             tolower(letter) == 'o' || tolower(letter) == 'u') {
             count++;
@@ -18,27 +18,32 @@ double calculateSize(const string& surname) {
 int main() {
     const int minLengh = 100;
     const int maxLengh = 130;
-    const int minWidth = 50;
+    const int minWidth = 50; // задаю цілочисельні костанти
     const int maxWidth = 100;
 
     string surname = "Seniv";
-    double letterSize = calculateSize(surname);
+    double letterSize = calculateSize(surname); // використовую виклик функції
 
     int minBannerLength = max(minLengh, static_cast<int>(ceil(letterSize)));
     int centralChars = minBannerLength / 2;
 
     cout << "Writing from left to right:" << endl;
-    for (int i = 0; i < centralChars; ++i) {
+
+    int i = 0;
+    do {
         cout << "X";
-    }
+        i++;
+    } while (i < centralChars);
     cout << surname << endl;
 
     cout << "Writing from right to left:" << endl;
-    cout << surname;
-    for (int i = 0; i < centralChars; ++i) {
+
+    i = 0;
+    do {                   //використовую цикл do while 
         cout << "X";
-    }
-    cout << endl;
+        i++;
+    } while (i < centralChars);
+    cout << surname << endl;
 
     return 0;
 }
