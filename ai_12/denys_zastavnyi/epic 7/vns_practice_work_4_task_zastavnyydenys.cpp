9492778e65a092ba156d2b0ca63ae592149208ba
zastavnyydenys <denys.zastavnyi.shi.2023@lpnu.ua>
2023-12-23 18:10:32 +0200

    epic 7 title uptade

--- ai_12/denys_zastavnyi/epic 7/vns_practice_work_4_task_zastavnyydenys.cpp
+++ ai_12/denys_zastavnyi/epic 7/vns_practice_work_4_task_zastavnyydenys.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <cstdarg>
-#include <fstream>
-#include <string>
 
 using namespace std;
 
-//13. в коді використано рекурсивну функцію
-double average(int amount, ...) {
-    va_list list;
-    va_start(list, amount);
-    double sum = 0;
+// 12 Перевантаження функції average для роботи з масивами
+double average(int amount, double numbers[]) {
+    double sum = 0;    //13. в коді використано рекурсивну функцію
     
     for (int i = 0; i < amount; i++) {
-        double x = va_arg(list, double);
-        sum += x;
+        sum += numbers[i];
     }
     
-    va_end(list);
     return sum / amount;
 }
 
 int main() {
-    double numbers[5];
+    double numbers[5];//6. створення одновимірного масиву для збереження 5 введених чисел 
     
-    // Введення чисел з клавіатури
     for (int i = 0; i < 5; i++) {
         cout << "Enter the number # " << i + 1 << " : ";
         cin >> numbers[i];
     }
 
-    // Знаходження та виведення середнього арифметичного
-    double averageValue = average(5, numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+    //9. в коді використаний while цикл
+    int i = 0;
+    while (i < 5) {
+        if (numbers[i] < 0) {
+            cout << "Negative number encountered. Exiting the loop." << endl;
+            break;    //11. в коді використано оператори break і continue
+
+        }
+
+        if (numbers[i] == 0) {
+            cout << "Zero encountered. Skipping to the next iteration." << endl;
+            i++;
+            continue;
+        }
+
+        cout << "Processing number: " << numbers[i] << endl;
+        i++;
+    }
+
+    // Знаходження та виведення середнього арифметичного з використанням перевантаженої функції
+    double averageValue = average(5, numbers);
     cout << "The average value is " << averageValue << endl;
+
     return 0;
 }
